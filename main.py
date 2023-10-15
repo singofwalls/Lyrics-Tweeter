2baeb9f8a65a3054ac8d11298ee8c6f10f1e89e2
Reece Mathews <mathewsreece@gmail.com>
2023-10-15 03:07:58 -0500

    strip contributors text

--- main.py
+++ main.py
@@ -51,7 +51,8 @@ EXTRANEOUS_TEXT = ["Translations.+\n", r"\[[a-zA-Z]+\]\n",
 # Or find or make a pull request to remove translation info from the HTML
                     "[0-9]+Embed", "EmbedShare URLCopyEmbedCopy", "Embed$",
                     "You might also like", r"See $BAND$ Live",
-                    "Get tickets as low as $[0-9]+", r"$SONG$ Lyrics"]
+                    "Get tickets as low as $[0-9]+", r"$SONG$ Lyrics",
+                    "[0-9]+ Contributors"]
 
 
 def get_lastfm_link(artist, track, l_creds):
